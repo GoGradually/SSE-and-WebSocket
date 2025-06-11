623e8dc32e91d7719e333bfde4551e8020d5792a
GoGradually <chkun3109@gmail.com>
2025-06-11 21:50:54 +0900

    SSE 학습/실험 및 테스트

---
+++ src/main/java/gogradually/sseandwebsocket/SseAndWebsocketApplication.java
@@ -0,0 +1,13 @@
+package gogradually.sseandwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseAndWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseAndWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gogradually/sseandwebsocket/SseAndWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package gogradually.sseandwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseAndWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
