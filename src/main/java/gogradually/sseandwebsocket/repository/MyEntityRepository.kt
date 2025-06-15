97fd12b73fc3da988c09d2f8f1da12aafb5f4731
GoGradually <chkun3109@gmail.com>
2025-06-15 23:55:42 +0900

    select for update 쿼리 추가

--- src/main/java/gogradually/sseandwebsocket/repository/MyEntityRepository.kt
+++ src/main/java/gogradually/sseandwebsocket/repository/MyEntityRepository.kt
@@ -4,8 +4,10 @@ import gogradually.sseandwebsocket.domain.MyEntity
 import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
 
 interface MyEntityRepository: JpaRepository<MyEntity, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select e from MyEntity e where e.id = :id")
     fun findByIdForUpdate(id: Long): MyEntity?
 }
